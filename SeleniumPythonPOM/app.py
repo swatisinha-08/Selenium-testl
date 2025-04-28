1600c632e3eca973348c2758f92fefad18b89c94
SWATI SINHA <108465361+swatisinha-08@users.noreply.github.com>
2025-04-28 16:31:59 +0530

    Update app.py

--- SeleniumPythonPOM/app.py
+++ SeleniumPythonPOM/app.py
@@ -1,3 +1,10 @@
-# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "This is a test app for deployment only."
+
 if __name__ == "__main__":
-    print("This is a test app for deployment only.")
+    app.run(host='0.0.0.0', port=8000)
