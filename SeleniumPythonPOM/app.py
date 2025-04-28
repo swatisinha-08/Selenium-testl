fce44bb5c19a77ec52e516f3e4c4685d6441889e
SWATI SINHA <108465361+swatisinha-08@users.noreply.github.com>
2025-04-28 16:36:15 +0530

    Update app.py

--- SeleniumPythonPOM/app.py
+++ SeleniumPythonPOM/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is a test app for deployment only."
+    return "Hello from Azure! This is your Python app running with Flask."
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
